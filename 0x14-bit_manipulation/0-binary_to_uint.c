80de4949cd66a93b37e8dd1ca42a82b2c92c7968
Phelix <118506052+phelixnyash@users.noreply.github.com>
2024-03-06 12:57:46 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,46 @@
-/*
- * File: 0-binary_to_uint.c
- */
-
-#include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: A string containing the binary representation
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: The converted unsigned int, or 0 if invalid input.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int result = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (*b)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b == '0' || *b == '1')
+		{
+			result = result * 2 + (*b - '0');
+			b++;
+		}
+		else
+		{
+			/* Invalid character in the string */
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		}
 	}
 
-	return (num);
+	return (result);
+}
+
+int main(void)
+{
+	const char *binary_number = "110101";
+	unsigned int result;
+
+	result = binary_to_uint(binary_number);
+
+	if (result != 0)
+		printf("Binary: %s, Unsigned Int: %u\n", binary_number, result);
+	else
+		printf("Invalid binary input.\n");
+
+	return (0);
 }
